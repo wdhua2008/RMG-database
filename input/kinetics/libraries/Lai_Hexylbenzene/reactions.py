7c6f45c9b7836b609e64d2d85156b8e92769ed8b
laitcl <laitcl@mit.edu>
2017-10-25 10:48:30 -0400

    Added a reaction Library with two reactions, named Lai_Hexylbenzene
    Two reactions added,
    
    1. 1-butyl radical + Styrene --> CCCCC[CH]C1C=CC=CC=1
    2. Hexylbenzene --> Toluene + 1-Pentene (Retroene Reaction)
    
    Calculations performed in CBS-QB3 levle of theory by Lawrence Lai and
    Max Liu

---
+++ input/kinetics/libraries/Lai_Hexylbenzene/reactions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+name = "Lai_Hexylbenzene"
+shortDesc = u"Reactions for Hexylbenzene pyrolysis at 450C"
+longDesc = u"""
+In conjunction with the Lai_Hexylbenzene thermo library,
+this reaction library contains CBS-QB3 calculations for reactions
+relevant to Hexylbenzene pyrolysis and supercritical water treatment.
+
+Lai, Lawrence, Gudiyella, Soumya, Liu, Mengjie, Green, William H. "Chemistry of Alkylaromatics Reconsidered". To be submitted to Energy and Fuels, 2017.
+
+Both calculations are done in CBS-QB3 level of theory.
+
+Specifics of the calculations performed:
+1. CBS-QB3 Level of theory was used after a B3LYP/6-311G(d,p) geometry optimization was performed
+2. CBS-QB3 Energy calculation was performed
+3. Frequency was calculated using B3LYP/CBSB7 iop(7/33=1) (Hessian was calculated)
+4. 1D Hindered Rotors were calculated for steps of 10 degrees up to the full 360 degree cycle, with geometry optimization on each step.
+5. All files generated were fed to Cantherm.
+6. Frequency scaling factor was 0.99
+7. Bond additivity corrections were not used.
+
+Disclaimer: The number of significant figures displayed does not reflect the accuracy of thermochemistry values. Sommers and Simmie esimates
+the error in enthalpy of formation (and therefore the activation energy) by CBS-QB3 calculations to be + or - 2.4kcal/mol 
+(http://pubs.acs.org/doi/abs/10.1021/acs.jpca.5b05448). 
+"""
+entry(
+    index = 1,
+    label = "butyl + styrene <=> rad1",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(236.006, 'cm^3/(mol*s)'), n=2.7878, Ea=(15.4228, 'kJ/mol'), T0=(1, 'K')),
+    shortDesc = u"Calculation performed by Lawrence Lai, April 2017, CBS-QB3 level of theory"
+)
+
+entry(
+    index = 2,
+    label = "Hexylbenzene <=> Toluene + 1-Pentene",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.62907e+07, 's^-1'), n=1.6863, Ea=(309.226, 'kJ/mol'), T0=(1, 'K')),
+    shortDesc = u"Calculation performed by Max Liu, CBS-QB3 level of theory"
+)
+
