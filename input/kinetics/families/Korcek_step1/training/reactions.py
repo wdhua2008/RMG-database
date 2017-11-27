6b977253905d8e231e589da22ab028a335de8bd4
Matt Johnson <mjohnson541@gmail.com>
2017-11-27 17:12:48 -0500

    korcek-step-1 rxn

--- input/kinetics/families/Korcek_step1/training/reactions.py
+++ input/kinetics/families/Korcek_step1/training/reactions.py
@@ -7,3 +7,20 @@ longDesc = u"""
 Put kinetic parameters for reactions to use as a training set for fitting
 group additivity values in this file.
 """
+entry(
+    index = 1,
+    label = "MPO1O3Q <=> MPO_CP13",
+    degeneracy = 4,
+    kinetics = Arrhenius(
+        A = (9.484e7, 's^-1'),
+        n = 0.489,
+        Ea = (30.624, 'kcal/mol'),
+        T0 = (1, 'K'),
+    ),
+    rank = 3,
+    shortDesc = u"""G4 calculation""",
+    longDesc = 
+u"""
+G4 calculation with 1-D HR by Lintao Bu at Nrel
+""",
+)
